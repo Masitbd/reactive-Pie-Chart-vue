d5c6960129de63c2f3c178663a3afff2f5c7797d
sarker24 <sarker24@gmail.com>
2023-08-13 20:01:33 +0200

    initial commit

---
+++ src/components/DataFatching.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted, reactive } from 'vue';
+
+const countries  = reactive([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all'); 
+    const data = await response.json();
+    countries.value = data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+</script>
+<template>
+    <div>
+      <h2>All Country List</h2>
+      <ul>
+        <li v-for="country in countries" :key="country.id">{{ country.name.common }}</li>
+      </ul>
+    </div>
+  </template>
+  
+ 
